42c214f0c382a8cda1073ab82b41ac82061b1210
Olivier Pédoussaut <opedoussaut@gmail.com>
2026-07-19 10:28:16 +0200

    Add Google Sheets configuration

---
+++ config.js
@@ -0,0 +1,14 @@
+window.LOFOTEN_CONFIG = {
+  googleClientId: "",
+  spreadsheetId: "1pNsRj7RHTSS2plAZLNTO4TV-PuzXcz7PK75_uj8HYl4",
+  sheets: {
+    settings: "Settings",
+    itinerary: "Itinerary",
+    activities: "Activities",
+    current: "Current_Status",
+    van: "Van_Log",
+    journal: "Journal",
+    gps: "GPS_Track",
+    expenses: "Expenses"
+  }
+};
