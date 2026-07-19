95322ed8d8b07949f535beb04d21b01b4bac36ce
Olivier Pédoussaut <opedoussaut@gmail.com>
2026-07-19 10:47:57 +0200

    Configure Google Apps Script backend URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.LOFOTEN_CONFIG = {
-  appsScriptUrl: "",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbzGbdnFxQByyRveG7RYMKz0tDggfCmjF8WbX4xVWehc-k-rKF91ALEN4XYo4YA0bQbt9A/exec",
   spreadsheetId: "1pNsRj7RHTSS2plAZLNTO4TV-PuzXcz7PK75_uj8HYl4"
 };
