ddf577e042d36842742e8dceb0f352e88b4a67ee
Olivier Pédoussaut <opedoussaut@gmail.com>
2026-07-19 10:40:53 +0200

    Switch configuration to Google Apps Script backend

--- config.js
+++ config.js
@@ -1,14 +1,4 @@
 window.LOFOTEN_CONFIG = {
-  googleClientId: "",
-  spreadsheetId: "1pNsRj7RHTSS2plAZLNTO4TV-PuzXcz7PK75_uj8HYl4",
-  sheets: {
-    settings: "Settings",
-    itinerary: "Itinerary",
-    activities: "Activities",
-    current: "Current_Status",
-    van: "Van_Log",
-    journal: "Journal",
-    gps: "GPS_Track",
-    expenses: "Expenses"
-  }
+  appsScriptUrl: "",
+  spreadsheetId: "1pNsRj7RHTSS2plAZLNTO4TV-PuzXcz7PK75_uj8HYl4"
 };
